9d0f486550b2828ec6ce5f5ec1e605d27299e96f
gjap18 <147403223+gjap18@users.noreply.github.com>
2023-11-14 12:11:06 +0100

    Added file testcities

---
+++ testcities.js
@@ -0,0 +1,33 @@
+var cities = 
+[
+    {
+    "country": "GB",
+    "name": "Ampthill",
+    "lat": "52.02694",
+    "lng": "-0.49567"
+  }, 
+  {
+    "country": "MX",
+    "name": "Santa Clara",
+    "lat": "18.97616",
+    "lng": "-98.3014"
+  },
+  {
+    "country": "DE",
+    "name": "Stuttgart",
+    "lat": "48.78232",
+    "lng": "9.17702"
+  },
+  {
+    "country": "IT",
+    "name": "Muscoline",
+    "lat": "45.56249",
+    "lng": "10.46187"
+  },
+  {
+    "country": "DE",
+    "name": "Furtwangen",
+    "lat": "48.05156",
+    "lng": "8.20715"
+  },
+]
