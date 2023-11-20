f757fd9e564bb2839077d6a2f3205f0e9579d9c3
Mattis D. <m.depoorter@web.de>
2023-11-20 17:36:32 +0100

    Added functioning randomizer

--- game_script.js
+++ game_script.js
@@ -1,15 +1,33 @@
+let buttonId = ["AnswerA", "AnswerB", "AnswerC", "AnswerD"];
+
 function getRandomCityName() {
 
-    var randomCity = cities[Math.floor(Math.random() * cities.length)];
+    let randomCity = cities[Math.floor(Math.random() * cities.length)];
     return randomCity.name; 
   }
   
   function displayRandomCityButton(buttonId) {
-    var cityName = getRandomCityName();
+
+    let cityName = getRandomCityName();
     document.getElementById(buttonId).innerText = "Antwort " + buttonId.charAt(buttonId.length - 1) + ": " + cityName;
+  }
+
+  function getRandomButton() {
+
+    let randomButton = buttonId[Math.floor(Math.random() * buttonId.length)]; 
+    return randomButton; 
   }
 
 displayRandomCityButton("AnswerA");
 displayRandomCityButton("AnswerB");
 displayRandomCityButton("AnswerC");
 displayRandomCityButton("AnswerD");
+
+let chosenOne = getRandomButton(); 
+console.log(chosenOne);  
+
+
+
+
+
+
