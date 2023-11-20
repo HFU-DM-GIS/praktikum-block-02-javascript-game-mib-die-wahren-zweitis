939389c52512a58cde9b85468a7597b59bab118e
Mattis D. <m.depoorter@web.de>
2023-11-20 20:41:02 +0100

    Added back slippy map. Wrote down thoughts.

--- game_script.js
+++ game_script.js
@@ -1,5 +1,13 @@
 let buttonId = ["AnswerA", "AnswerB", "AnswerC", "AnswerD"];
 
+/*Use these to change BBox. TODO: Write funktion that changes src of slippy map. Standard format: bbox = left,bottom,right,top
+                                                                                                  bbox = min Longitude , min Latitude , max Longitude , max Latitude */
+let bBoxLeft
+let bBoxBottom
+let bBoxRight
+let bBoxTop
+
+//Modify this to also return lat, lng of the city. These are needed to direct slippy map to correct tile. 
 function getRandomCityName() {
 
     let randomCity = cities[Math.floor(Math.random() * cities.length)];
@@ -12,22 +20,21 @@ function getRandomCityName() {
     document.getElementById(buttonId).innerText = "Antwort " + buttonId.charAt(buttonId.length - 1) + ": " + cityName;
   }
 
-  function getRandomButton() {
+  //This is supposed to designate one button as the right answer. Works as it should. 
+  /*function getRandomButton() {
 
     let randomButton = buttonId[Math.floor(Math.random() * buttonId.length)]; 
     return randomButton; 
-  }
+  } */
 
 displayRandomCityButton("AnswerA");
 displayRandomCityButton("AnswerB");
 displayRandomCityButton("AnswerC");
 displayRandomCityButton("AnswerD");
 
-let chosenOne = getRandomButton(); 
-console.log(chosenOne);  
+/*let chosenOne = getRandomButton(); 
+console.log(chosenOne);'*/   
 
-
-
-
-
-
+/* This is pseudo-code only used to remember tasks:
+1. Write funtion to count the score. Needs a score box as HTML first.
+*/ 
