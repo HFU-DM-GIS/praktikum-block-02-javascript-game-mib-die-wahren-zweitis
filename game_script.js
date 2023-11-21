ca745e65f93c661b8ef2ccb8635442e0e3793517
Mattis D. <m.depoorter@web.de>
2023-11-21 12:28:03 +0100

    Implemented shuffle algorythm.

--- game_script.js
+++ game_script.js
@@ -1,4 +1,6 @@
 let buttonId = ["AnswerA", "AnswerB", "AnswerC", "AnswerD"];
+var cityIndex = 0; 
+let cityIndicies = []; 
 
 /*Use these to change BBox. TODO: Write funktion that changes src of slippy map. Standard format: bbox = left,bottom,right,top
                                                                                                   bbox = min Longitude , min Latitude , max Longitude , max Latitude */
@@ -7,10 +9,11 @@ let bBoxBottom
 let bBoxRight
 let bBoxTop
 
-//Modify this to also return lat, lng of the city. These are needed to direct slippy map to correct tile. 
+//Modify this to also return lat, lng of the city. These are needed to direct slippy map to correct tile. Also change so no citys can be displayed twice.  
 function getRandomCityName() {
 
-    let randomCity = cities[Math.floor(Math.random() * cities.length)];
+    let randomCity = cities[cityIndicies[cityIndex]];
+    cityIndex++; 
     return randomCity.name; 
   }
   
@@ -20,12 +23,42 @@ function getRandomCityName() {
     document.getElementById(buttonId).innerText = "Antwort " + buttonId.charAt(buttonId.length - 1) + ": " + cityName;
   }
 
+  function initRandomCityList(){
+
+    //Get number of cities in list
+    let numCitys = cities.length; 
+    //Generate array of city indices
+    cityIndicies = Array.from(Array(numCitys).keys()); 
+    //Shuffle indicies 
+    shuffle(cityIndicies); 
+    
+  }
+//Copied from https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+  function shuffle(array) {
+    let currentIndex = array.length,  randomIndex;
+  
+    // While there remain elements to shuffle.
+    while (currentIndex > 0) {
+  
+      // Pick a remaining element.
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+  
+      // And swap it with the current element.
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
+    }
+  
+    return array;
+  }
+
   //This is supposed to designate one button as the right answer. Works as it should. 
   /*function getRandomButton() {
 
     let randomButton = buttonId[Math.floor(Math.random() * buttonId.length)]; 
     return randomButton; 
   } */
+initRandomCityList(); 
 
 displayRandomCityButton("AnswerA");
 displayRandomCityButton("AnswerB");
@@ -37,4 +70,5 @@ console.log(chosenOne);'*/
 
 /* This is pseudo-code only used to remember tasks:
 1. Write funtion to count the score. Needs a score box as HTML first.
+
 */ 
