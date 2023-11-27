3e4c4216faa246142268a8e5bafa6d577585e966
gjap18 <147403223+gjap18@users.noreply.github.com>
2023-11-27 11:33:41 +0100

    added more testcities

--- testcities.js
+++ testcities.js
@@ -1,69 +1,39 @@
 var cities = 
 [
-    {
-    "country": "GB",
-    "name": "Ampthill",
-    "lat": 52.02694,
-    "lng": -0.49567
-  }, 
   {
-    "country": "MX",
-    "name": "Santa Clara",
-    "lat": "18.97616",
-    "lng": "-98.3014"
+    "country": "AE",
+    "name": "Dubai",
+    "lat": 25.07725,
+    "lng": 55.30927
   },
   {
-    "country": "DE",
-    "name": "Stuttgart",
-    "lat": "48.78232",
-    "lng": "9.17702"
+    "country": "AE",
+    "name": "Abu Dhabi",
+    "lat": 24.45118,
+    "lng": 54.39696
   },
   {
-    "country": "IT",
-    "name": "Muscoline",
-    "lat": "45.56249",
-    "lng": "10.46187"
+    "country": "AT",
+    "name": "Wien",
+    "lat": 48.20849,
+    "lng": 16.37208
   },
   {
-    "country": "DE",
-    "name": "Furtwangen",
-    "lat": "48.05156",
-    "lng": "8.20715"
+    "country": "AT",
+    "name": "Thüringen",
+    "lat": 47.2,
+    "lng": 9.76667
   },
   {
-    "country": "AD",
-    "name": "Sant Julià de Lòria",
-    "lat": "42.46372",
-    "lng": "1.49129"
+    "country": "AT",
+    "name": "Salzburg",
+    "lat": 47.79941,
+    "lng": 13.04399
   },
   {
-    "country": "AD",
-    "name": "Pas de la Casa",
-    "lat": "42.54277",
-    "lng": "1.73361"
-  },
-  {
-    "country": "AD",
-    "name": "Ordino",
-    "lat": "42.55623",
-    "lng": "1.53319"
-  },
-  {
-    "country": "AD",
-    "name": "les Escaldes",
-    "lat": "42.50729",
-    "lng": "1.53414"
-  },
-  {
-    "country": "AD",
-    "name": "la Massana",
-    "lat": "42.54499",
-    "lng": "1.51483"
-  },
-  {
-    "country": "AD",
-    "name": "Encamp",
-    "lat": "42.53474",
-    "lng": "1.58014"
-  },
+    "country": "AU",
+    "name": "Sydney",
+    "lat": -33.86785,
+    "lng": S151.20732
+    },
 ]
