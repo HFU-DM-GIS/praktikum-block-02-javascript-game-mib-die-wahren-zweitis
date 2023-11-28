a556c916193559e4688cb385b4ebd2da35c595ae
gjap18 <147403223+gjap18@users.noreply.github.com>
2023-11-28 11:38:29 +0100

    added more citites to testcities

--- testcities.js
+++ testcities.js
@@ -31,9 +31,202 @@ var cities =
     "lng": 13.04399
   },
   {
-    "country": "AU",
+    "country": "CA",
     "name": "Sydney",
-    "lat": -33.86785,
-    "lng": S151.20732
+    "lat": 46.1351,
+    "lng": -60.1831
+  },
+    {
+      "country": "BE",
+      "name": "Brussels",
+      "lat": 50.85045,
+      "lng": 4.34878
     },
+    {
+      "country": "DE",
+      "name": "Hamburg",
+      "lat": 53.55073,
+      "lng": 9.99302
+    },
+    {
+      "country": "DE",
+      "name": "Berlin",
+      "lat": 52.52437,
+      "lng": 13.41053
+    },
+    {
+      "country": "DE",
+      "name": "Stuttgart",
+      "lat": 48.78232,
+      "lng": 9.17702
+    },
+    {
+      "country": "DE",
+      "name": "Furtwangen",
+      "lat": 48.05156,
+      "lng": 8.20715
+    },
+    {
+      "country": "DE",
+      "name": "Freiburg",
+      "lat": 47.9959,
+      "lng": 7.85222
+    },
+    {
+      "country": "DE",
+      "name": "Dresden",
+      "lat": 51.05089,
+      "lng": 13.73832
+    },
+    {
+      "country": "DE",
+      "name": "Leipzig",
+      "lat": 51.33962,
+      "lng": 12.37129
+    },
+    {
+      "country": "DE",
+      "name": "Bremen",
+      "lat": 53.07582,
+      "lng": 8.80717
+    },
+    {
+      "country": "IE",
+      "name": "Dublin",
+      "lat": 53.33306,
+      "lng": -6.24889
+    },
+    {
+      "country": "IE",
+      "name": "Limerick",
+      "lat": 52.66472,
+      "lng": -8.62306
+    },
+    {
+      "country": "GB",
+      "name": "London",
+      "lat": 51.50853,
+      "lng": -0.12574
+    },
+    {
+      "country": "GB",
+      "name": "Edinburgh",
+      "lat": 55.95206,
+      "lng": -3.19648
+    },
+    {
+      "country": "FR",
+      "name": "Paris",
+      "lat": 48.85341,
+      "lng": 2.3488
+    },
+    {
+      "country": "IT",
+      "name": "Rome",
+      "lat": 41.89193,
+      "lng": 12.51133
+    },
+    {
+      "country": "ES",
+      "name": "Barcelona",
+      "lat": 41.38879,
+      "lng": 2.15899
+    },
+    {
+      "country": "JP",
+      "name": "Tokyo",
+      "lat": 35.6895,
+      "lng": 139.69171
+    },
+    {
+      "country": "PT",
+      "name": "Lisbon",
+      "lat": 38.71667,
+      "lng": -9.13333
+    },
+    {
+      "country": "TR",
+      "name": "Istanbul",
+      "lat": 41.01384,
+      "lng": 28.94966
+    },
+    {
+      "country": "TR",
+      "name": "Antalya",
+      "lat": 36.90812,
+      "lng": 30.69556
+    },
+    {
+      "country": "TR",
+      "name": "Ankara",
+      "lat": 39.91987,
+      "lng": 32.85427
+    },
+    {
+      "country": "TH",
+      "name": "Bangkok",
+      "lat": 13.75398,
+      "lng": 100.50144
+    },
+    {
+      "country": "TH",
+      "name": "Phuket",
+      "lat": 7.89059,
+      "lng": 98.3981
+    },
+    {
+      "country": "EG",
+      "name": "Cairo",
+      "lat": 30.06263,
+      "lng": 31.24967
+    },
+    {
+      "country": "FI",
+      "name": "Helsinki",
+      "lat": 60.16952,
+      "lng": 24.93545
+    },
+    {
+      "country": "NO",
+      "name": "Oslo",
+      "lat": 59.91273,
+      "lng": 10.74609
+    },
+    {
+      "country": "LV",
+      "name": "Riga",
+      "lat": 56.946,
+      "lng": 24.10589
+    },
+    {
+      "country": "LT",
+      "name": "Vilnius",
+      "lat": 54.68916,
+      "lng": 25.2798
+    },
+    {
+      "country": "HU",
+      "name": "Budapest",
+      "lat": 47.49835,
+      "lng": 19.04045
+    },
+    {
+      "country": "NL",
+      "name": "Amsterdam",
+      "lat": 52.37403,
+      "lng": 4.88969
+    },
+    {
+      "country": "MT",
+      "name": "Valletta",
+      "lat": 35.89968,
+      "lng": 14.5148
+    },
+    {
+      "country": "US",
+      "name": "New York City",
+      "lat": 40.71427,
+      "lng": -74.00597
+    },
+    
 ]
